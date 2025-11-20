fe3fcff62d20037f0cf119c132cddbad5ef0fe73
Andrea Marrano <andrea.marrano@autoxy.it>
2025-11-20 23:11:49 +0100

    add some custom snipper

--- q1-shop-stripe-alert.php
+++ q1-shop-stripe-alert.php
@@ -105,3 +105,93 @@ add_filter('woocommerce_loop_add_to_cart_link', function($html, $product){
     foreach ($attrs as $k=>$v) $attr .= sprintf(' %s="%s"', esc_attr($k), esc_attr($v));
     return sprintf('<a%s>%s</a>', $attr, $text);
 }, 99, 2);
+
+//Custom Snippets
+/**
+ * Change "Out Of Stock" text.
+ *
+ */
+function your_prefix_change_out_stock_string() {
+	return __( 'Esaurito', 'your-text-domain' );
+}
+add_filter( 'shoptimizer_shop_out_of_stock_string', 'your_prefix_change_out_stock_string' );
+
+add_filter( 'woocommerce_cart_item_price', 'cg_cart_table_price_display', 30, 3 );
+  
+function cg_cart_table_price_display( $price, $values, $cart_item_key ) {
+   $slashed_price = $values['data']->get_price_html();
+   $is_on_sale = $values['data']->is_on_sale();
+   if ( $is_on_sale ) {
+      $price = $slashed_price;
+   }
+   return $price;
+}
+
+if ( ! function_exists( 'shoptimizer_mini_cart_total_discounts' ) ) {
+    function shoptimizer_mini_cart_total_discounts()  {
+        global $woocommerce;
+        $discount_total = 0;
+ 
+        foreach ($woocommerce->cart->get_cart() as $cart_item_key => $values) {
+ 
+            $_product = $values['data'];
+ 
+            if ($_product->is_on_sale()) {
+                $regular_price = $_product->get_regular_price();
+                $sale_price = $_product->get_sale_price();
+ 
+                if (empty($regular_price)){ //then this is a variable product
+                    $available_variations = $_product->get_available_variations();
+                    $variation_id=$available_variations[0]['variation_id'];
+                    $variation= new WC_Product_Variation( $variation_id );
+                    $regular_price = $variation ->regular_price;
+                    $sale_price = $variation ->sale_price;
+                }
+ 
+                $discount = ceil(($regular_price - $sale_price) * $values['quantity'] );
+                $discount_total += $discount;
+            }
+ 
+        }
+        if ($discount_total > 0) { ?>
+            <p class="woocommerce-mini-cart__total total discounts-total">
+                <strong><?php echo esc_html__( 'You save', 'shoptimizer' ); ?></strong>
+                <span class="woocommerce-Price-amount amount">-<?php echo wc_price($discount_total + $woocommerce->cart->discount_cart); ?></span>
+            </p>
+
+			<style>
+				.widget_shopping_cart p.total.discounts-total {
+					color: green;
+					font-size: 13px;
+					margin-bottom: -1.2em;
+					order: 1;
+					border-top: 1px solid #e2e2e2;
+
+				}
+				.widget_shopping_cart p.total.discounts-total strong {
+					font-weight: normal;
+				}
+				.shoptimizer-mini-cart-wrap .widget_shopping_cart .discounts-total .amount {
+					margin: 0;
+					font-weight: normal;
+					color: green;
+				}
+				.shoptimizer-mini-cart-wrap .widget_shopping_cart .discounts-total .amount bdi {
+					color: green;
+				}
+				.widget_shopping_cart p.total {
+					order: 2;
+					border: none;
+				}
+				.widget_shopping_cart p.buttons {
+					order: 3;
+				}
+				.shoptimizer-mini-cart-wrap .cart-drawer-below {
+					order: 4;
+				}
+			</style>
+        <?php }
+    }
+}
+
+add_action( 'woocommerce_widget_shopping_cart_before_buttons',  'shoptimizer_mini_cart_total_discounts', 10);
